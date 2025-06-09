600abc4fd1e40626fda277718134b1729edfc3f2
analiticgpt <analiticgpt@gmail.com>
2025-06-09 17:07:30 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,16 +1,1 @@
-const express = require('express');
-const cors = require('cors');
 
-const app = express();
-const PORT = process.env.PORT || 3000;
-
-app.use(cors());
-
-// Прокси для ElevenLabs
-app.get('/widget', async (req, res) => {
-  res.send('🎯 Прокси работает! Можешь вставлять эту ссылку в <script src="...">');
-});
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
